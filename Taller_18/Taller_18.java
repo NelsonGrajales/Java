68e6214e6217c17ddf539c26d4b747e0e920f2a8
NelsonGrajales <ngrajales2332@gmail.com>
2024-05-19 22:01:49 -0500

    taller 18

--- Taller_18/Taller_18.java
+++ Taller_18/Taller_18.java
@@ -4,22 +4,23 @@ import javax.swing.JOptionPane;
 
 public class Taller_18 {
 
-    public  String leer() {
+    public String leer() {
         int n = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número de elementos del vector: "));
-        int nuevoVal,pos;
+        int nuevoVal, pos;
         int[] vector = new int[n];
         String salida = "Dimension del vector: " + n + "\n";
         for (int x = 0; x < n; x++) {
             vector[x] = Integer.parseInt(JOptionPane.showInputDialog("Vec[" + (x + 1) + "]"));
             if (x == n - 1)
-                salida += "vec[" + (x+1) + "] = " + vector[x];
+                salida += "vec[" + (x + 1) + "] = " + vector[x];
             else
-                salida += "vec[" + (x+1) + "] = " + vector[x] + "\n";
+                salida += "vec[" + (x + 1) + "] = " + vector[x] + "\n";
         }
 
         nuevoVal = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un nuevo elemento: "));
         do {
-            pos = Integer.parseInt(JOptionPane.showInputDialog("ingrese la poscion para el nuevo elemento (menor a " + (n+1)+"):"));
+            pos = Integer.parseInt(JOptionPane
+                    .showInputDialog("ingrese la poscion para el nuevo elemento (menor a " + (n + 1) + "):"));
         } while (pos > n || pos < 0);
 
         salida += "\nNuevo elemento: " + nuevoVal + "\nPosicion Elemento: " + pos + "\n";
@@ -28,16 +29,15 @@ public class Taller_18 {
 
         for (int x = 0; x < n; x++) {
             if (x == n - 1)
-                salida += "vec[" + (x+1)+ "] = " + vector[x];
+                salida += "vec[" + (x + 1) + "] = " + vector[x];
             else
-            salida += "vec[" + (x+1) + "] = " + vector[x] + "\n";
+                salida += "vec[" + (x + 1) + "] = " + vector[x] + "\n";
         }
 
         return salida;
     }
 
     public void procesar(int vector[], int nuevoVal, int pos) {
-        vector[pos-1] = nuevoVal;
+        vector[pos - 1] = nuevoVal;
     }
 }
-
