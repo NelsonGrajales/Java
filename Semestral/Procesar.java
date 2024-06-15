b789445a43aad22eb9f1ba997362a40e7a2899c7
NelsonGrajales <ngrajales2332@gmail.com>
2024-06-15 01:20:33 -0500

    semestral

--- Semestral/Procesar.java
+++ Semestral/Procesar.java
@@ -6,25 +6,59 @@ public class Procesar {
                 vec[x][j] = 0;
             }
         }
-        for (int x = n - 1; x > 0; x--) {
+        for (int x = n - 1; x > -1; x--) {
+            int mitad = n / 2;
+            if (n % 2 != 0 && n >= 5) {
+                vec[mitad][mitad] = n * n;
+                vec[mitad - 1][mitad] = (n * n) - 1;
+            }
             if (h == 1) {
                 if (h < n) {
                     for (int j = 0; j < n; j++) {
                         vec[j][x] = h;
                         h++;
                     }
-                } else if (h == n + 1) {
-                    for (int j = n - 2; j > -1; j--) {
-                        vec[x][j] = h;
-                        h++;
+                    if (h > n) {
+                        for (int j = n - 2; j > -1; j--) {
+                            vec[x][j] = h;
+                            h++;
+                        }
                     }
                 }
+            } else if (x == 0) {
+                int z = n - 3;
+                for (int j = 0; j <= n - 2; j++) {
+                    vec[x][j] = h;
+                    h++;
+                }
+                for (int y = 1; y < n - 1; y++) {
+                    vec[y][n - 2] = h;
+                    h++;
+                }
+                while (z > 0) {
+                    vec[n - 2][z] = h;
+                    z--;
+                    h++;
+                }
+                for (int y = mitad; y > 0; y--) {
+                    vec[y][1] = h;
+                    h++;
+                }
+            } else {
+                vec[x][0] = h;
+                h++;
             }
+
         }
 
         for (int x = 0; x < n; x++) {
             for (int j = 0; j < n; j++) {
-                salida += "[" + (vec[x][j]) + "]";
+                if (vec[x][j] < 10) {
+                    salida += "[0" + (vec[x][j]) + "]";
+                } else {
+                    salida += "[" + (vec[x][j]) + "]";
+
+                }
             }
             salida += "\n";
         }
