bd7f298f54792de251c78d066f0d2977c4d8cfcb
NelsonGrajales <ngrajales2332@gmail.com>
2024-06-21 23:30:38 -0500

    correccion

--- Semestral/Leer.java
+++ Semestral/Leer.java
@@ -10,10 +10,29 @@ public class Leer {
         int vec[][] = new int[n][n];
         String salida = "Dimension de la matriz: " + n;
 
-        for (int x = 0; x < n; x++) {
-            for (int j = 0; j < n; j++) {
-                vec[x][j] = 0;
+        int t = 0;
+        int h = 1;
+        int u = n - 1;
+        while (t < n) {
+            t = 0;
+            for (int x = u; x < n; x++) {
+                vec[t][x] = h;
+                h++;
+                t++;
             }
+            u--;
+        }
+        u = n - 1;
+        int r = 1;
+        while (r <= n - 1) {
+            t = r;
+            for (int x = 0; x < u; x++) {
+                vec[t][x] = h;
+                h++;
+                t++;
+            }
+            u--;
+            r++;
         }
 
         salida += "\n Matriz Original\n";
--- Semestral/Procesar.java
+++ Semestral/Procesar.java
@@ -1,29 +1,5 @@
 public class Procesar {
     public void procesar(int vec[][], String salida, int n) {
-        int t = 0;
-        int h = 1;
-        int u = n - 1;
-        while (t < n) {
-            t = 0;
-            for (int x = u; x < n; x++) {
-                vec[t][x] = h;
-                h++;
-                t++;
-            }
-            u--;
-        }
-        u = n - 1;
-        int r = 1;
-        while (r <= n - 1) {
-            t = r;
-            for (int x = 0; x < u; x++) {
-                vec[t][x] = h;
-                h++;
-                t++;
-            }
-            u--;
-            r++;
-        }
 
         for (int x = 0; x < n; x++) {
             for (int j = 0; j < n; j++) {
